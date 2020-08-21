7ca87f684288595af96736a287c0e06f165b1abb
enrico <enrico.stemmer@gmx.de>
2020-08-21 23:13:05 +0200

    recreate

--- src/logic/ImageLoader.java
+++ src/logic/ImageLoader.java
@@ -11,31 +11,26 @@ import javax.imageio.ImageIO;
 import world.World.Layers;
 
 public class ImageLoader {
-
-	public static Map<Layers, BufferedImage> loadImage(String name, Layers layer) {
-		Map<Layers, BufferedImage> temp = new HashMap<Layers, BufferedImage>();
+	
+	public static Map<Layers, BufferedImage> loadImage(String name,Layers layer) {
+		Map<Layers, BufferedImage> temp = new HashMap<Layers,BufferedImage>();
 		try {
 			switch (layer) {
 			case Floor:
-				temp.put(layer, ImageIO.read(new File("rsc/floor pictures/" + name + ".png")));
+				temp.put(layer,ImageIO.read(new File("rsc/floor pictures/" + name + ".png")));
 				break;
 			case Floordecoration:
-				temp.put(layer, ImageIO.read(new File("rsc/floordecoration pictures/" + name + ".png")));
+				temp.put(layer,ImageIO.read(new File("rsc/floordecoration pictures/" + name + ".png")));
 				break;
 			case Objects:
-				temp.put(layer, ImageIO.read(new File("rsc/objekt pictures/" + name + ".png")));
+				temp.put(layer,ImageIO.read(new File("rsc/objekt pictures/" + name + ".png")));
 				break;
 			case Effects:
-				temp.put(layer, ImageIO.read(new File("rsc/effects pictures/" + name + ".png")));
+				temp.put(layer,ImageIO.read(new File("rsc/effects pictures/" + name + ".png")));
 				break;
 			}
-			// Was würdest du dann machen wenn wir ein Tile animieren wollen, da brauchen
-			// wir dann ja mehrere Bilder, die dann nicht alle auf diese Weise geladen
-			// werden können? -Jan
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		} catch (IOException e) {e.printStackTrace();}
 		return temp;
 	}
-
+	
 }
--- src/logic/Main.java
+++ src/logic/Main.java
@@ -5,22 +5,16 @@ import logic.Statemanager.States;
 import world.World;
 
 public class Main {
-
-	public static Statemanager stateManager;
+	
+	public static Statemanager statemanager;
 	public static World world;
-	public static ClassJFrame gameFrame;
-
+	public static ClassJFrame frame;
+	
 	public Main() {
-<<<<<<< HEAD
 		statemanager = new Statemanager();
 		frame = new ClassJFrame();
 		statemanager.setState(States.mainmenu);
 		//world = new World();
-=======
-		stateManager = new Statemanager();
-		gameFrame = new ClassJFrame();
-		// world = new World();
->>>>>>> 4d4f60ed9fafffb2b90637841b303df9e224796b
 	}
-
+	
 }
