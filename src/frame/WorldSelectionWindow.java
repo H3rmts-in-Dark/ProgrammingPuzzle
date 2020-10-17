2585d98d9bc0341c73ec2263908a29a9b74854d8
ManInDark <janknd13@gmail.com>
2020-10-17 23:49:42 +0200

    .

--- src/frame/WorldSelectionWindow.java
+++ src/frame/WorldSelectionWindow.java
@@ -13,7 +13,7 @@ public class WorldSelectionWindow extends CustomWindow {
 	private static final String title = "Worldselection";
 
 	public WorldSelectionWindow() {
-		super(SCALEFACTOR * 10, SCALEFACTOR * 11, new Point(0, 0), title);
+		super(SCALEFACTOR * 8, SCALEFACTOR * 9, new Point(0, 0), title);
 	}
 
 	@Override
@@ -22,9 +22,24 @@ public class WorldSelectionWindow extends CustomWindow {
 		Graphics2D g2 = image.createGraphics();
 		g2.setColor(Color.GRAY);
 		g2.fillRect(0, 0, getWidth(), getHeight());
-		
+
+		for (int y = 0; y < 5; y++) {
+			for (int x = 0; x < 5; x++) {
+				// draw((SCALEFACTOR / 2) * (1 + 2 * x), (SCALEFACTOR / 2) * (1 + 2 * y),
+				// SCALEFACTOR, SCALEFACTOR)
+				// nicht richtige funktion, aber die wird dann in etwa benutzt
+			}
+		}
 		return image;
 	}
+
+	// um die ausgewählte welt herauszufinden:
+	// for(int y = 0; y < 5; y++) {
+	// 	for(int x = 0; x < 5; x++) {
+	// 		new Rectangle((SCALEFACTOR / 2) * (1 + 2 * x), (SCALEFACTOR / 2) * (1 + 2 * y),
+	// 			SCALEFACTOR, SCALEFACTOR).contains(<Mouseposition>)
+	// 	}
+	// }
 
 	@Override
 	public void drawCursor(Graphics2D g2, Point point) {
